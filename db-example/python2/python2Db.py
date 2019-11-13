601fe08609f1f954dab1a5118b6dcbe33f61f1ed
life_inner_challenge <xxuchuanhao@163.com>
2019-11-14 00:20:17 +0800

    update

--- db-example/python2/python2Db.py
+++ db-example/python2/python2Db.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
-import sys
-sys.path.append('.')
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import MySQLdb
 import traceback
 
